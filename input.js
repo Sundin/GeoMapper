dae36706cf877ce249c905d5d9a38733e6aab367
Gustav Sundin <sundin666@gmail.com>
2019-02-02 15:34:08 +0100

    Added button to upload csv file

--- input.js
+++ input.js
@@ -1,3 +1,32 @@
+function startRead(evt) {
+    var file = document.getElementById('file').files[0];
+    if (file) {
+        getAsText(file);
+    }
+}
+
+function getAsText(readFile) {
+    var reader = new FileReader();
+    reader.readAsText(readFile, "UTF-8");
+    reader.onload = loaded;
+}
+
+function loaded(evt) {
+    processData(evt.target.result);
+}
+
+function processData(csvData) {
+    const lines = readFromCsv(csvData);
+    console.table(lines);
+
+    for (var i = 0; i < lines.length; i++) {
+        const item = lines[i];
+        getMarker(item, geocoder).then(marker => {
+            marker.addTo(map);
+        });
+    };
+}
+
 function readFromCsv(allText) {
     var lines = [];
     var allTextLines = allText.split(/\r\n|\n/);
