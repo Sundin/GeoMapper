491d329957001b1b09860efd8b79574d4fd3573d
Gustav Sundin <sundin666@gmail.com>
2019-11-06 18:09:57 +0100

    Change column names

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ A tool for displaying a bunch of locations on a map in your browser.
 
 Try it out at: https://sundin.github.io/GeoMapper/
 
-Just upload a valid CSV file (it needs to include two columns named `Shipping Address 1` and `Shipping City` at the minimum) and you are good to go!
+Just upload a valid CSV file (it needs to include two columns named `Address` and `City` at the minimum) and you are good to go!
+
+## Local Development
+
+To try it out locally, simply open [index.html](./index.html) in your browser of choice.
--- marker.js
+++ marker.js
@@ -1,5 +1,5 @@
 function getMarker(item, geocoder) {
-  const searchQuery = item["Shipping Address"] + " " + item["Shipping City"];
+  const searchQuery = item["Address"] + " " + item["City"];
 
   return new Promise((resolve, reject) => {
     geocoder.geocode(searchQuery, function(results) {
@@ -8,7 +8,7 @@ function getMarker(item, geocoder) {
         return resolve(L.marker(r.center).bindPopup(r.name));
       } else {
         // Fallback to only city if street address not found
-        const backupSearchQuery = item["Shipping City"];
+        const backupSearchQuery = item["City"];
         geocoder.geocode(backupSearchQuery, function(results) {
           var r = results[0];
           if (r) {
