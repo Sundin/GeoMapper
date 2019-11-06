b455aa4101400e9884e8aaa2cab51748c1bf26b6
Gustav Sundin <sundin666@gmail.com>
2019-11-06 18:08:16 +0100

    Indent using 2 spaces

--- input.js
+++ input.js
@@ -1,52 +1,57 @@
 function startRead(evt) {
-    var file = document.getElementById('file').files[0];
-    if (file) {
-        getAsText(file);
-    }
+  var file = document.getElementById("file").files[0];
+  if (file) {
+    getAsText(file);
+  }
 }
 
 function getAsText(readFile) {
-    var reader = new FileReader();
-    reader.readAsText(readFile, "UTF-8");
-    reader.onload = loaded;
+  var reader = new FileReader();
+  reader.readAsText(readFile, "UTF-8");
+  reader.onload = loaded;
 }
 
 function loaded(evt) {
-    processData(evt.target.result);
+  processData(evt.target.result);
 }
 
 function processData(csvData) {
-    document.getElementById("warning-section").style.display = "none";
-    document.getElementById("warning-list").innerHTML = "";
+  document.getElementById("warning-section").style.display = "none";
+  document.getElementById("warning-list").innerHTML = "";
 
-    const lines = readFromCsv(csvData);
+  const lines = readFromCsv(csvData);
 
-    lines.forEach(item => {
-        getMarker(item, geocoder).then(marker => {
-            marker.addTo(map);
-        }).catch(searchQuery => {
-            document.getElementById("warning-section").style.display = "";
-            const warningHtml = "♠ " + searchQuery + "<br/>";
-            document.getElementById("warning-list").innerHTML += warningHtml;
-        });
-    });
+  lines.forEach(item => {
+    getMarker(item, geocoder)
+      .then(marker => {
+        marker.addTo(map);
+      })
+      .catch(searchQuery => {
+        document.getElementById("warning-section").style.display = "";
+        const warningHtml = "♠ " + searchQuery + "<br/>";
+        document.getElementById("warning-list").innerHTML += warningHtml;
+      });
+  });
 }
 
 function readFromCsv(allText) {
-    var lines = [];
-    var allTextLines = allText.split(/\r\n|\n/);
-    var headers = allTextLines[0].split(',');
+  var lines = [];
+  var allTextLines = allText.split(/\r\n|\n/);
+  var headers = allTextLines[0].split(",");
 
-    for (var i = 1; i < allTextLines.length; i++) {
-        var data = allTextLines[i].split(',');
-        if (data.length == headers.length) {
-            const item = {};
-            for (var j = 0; j < headers.length; j++) {
-                item[headers[j].replace(/^\"+|\"+$/g, '')] = data[j].replace(/^\"+|\"+$/g, '');;
-            }
-            lines.push(item);
-        }
+  for (var i = 1; i < allTextLines.length; i++) {
+    var data = allTextLines[i].split(",");
+    if (data.length == headers.length) {
+      const item = {};
+      for (var j = 0; j < headers.length; j++) {
+        item[headers[j].replace(/^\"+|\"+$/g, "")] = data[j].replace(
+          /^\"+|\"+$/g,
+          ""
+        );
+      }
+      lines.push(item);
     }
+  }
 
-    return lines;
+  return lines;
 }
--- marker.js
+++ marker.js
@@ -1,23 +1,23 @@
 function getMarker(item, geocoder) {
-    const searchQuery = item['Shipping Address 1'] + ' ' + item['Shipping City'];
+  const searchQuery = item["Shipping Address"] + " " + item["Shipping City"];
 
-    return new Promise((resolve, reject) => {
-        geocoder.geocode(searchQuery, function (results) {
-            var r = results[0];
-            if (r) {
-                return resolve(L.marker(r.center).bindPopup(r.name));
-            } else {
-                // Fallback to only city if street address not found
-                const backupSearchQuery = item['Shipping City']
-                geocoder.geocode(backupSearchQuery, function (results) {
-                    var r = results[0];
-                    if (r) {
-                        return resolve(L.marker(r.center).bindPopup(r.name));
-                    } else {
-                        return reject(searchQuery);
-                    }
-                });
-            }
+  return new Promise((resolve, reject) => {
+    geocoder.geocode(searchQuery, function(results) {
+      var r = results[0];
+      if (r) {
+        return resolve(L.marker(r.center).bindPopup(r.name));
+      } else {
+        // Fallback to only city if street address not found
+        const backupSearchQuery = item["Shipping City"];
+        geocoder.geocode(backupSearchQuery, function(results) {
+          var r = results[0];
+          if (r) {
+            return resolve(L.marker(r.center).bindPopup(r.name));
+          } else {
+            return reject(searchQuery);
+          }
         });
+      }
     });
+  });
 }
