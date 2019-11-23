765e9b9087e8d1f7b838fcd958343160f2dcfb32
Gustav Sundin <sundin666@gmail.com>
2019-11-23 15:02:29 +0100

    Add categories to map

--- input.js
+++ input.js
@@ -17,6 +17,8 @@ function loaded(evt) {
 
 // window.addEventListener("DOMContentLoaded", loadFromGoogleSheets);
 
+let categories = {};
+
 function loadFromGoogleSheets(data) {
   document.getElementById("warning-section").style.display = "none";
   document.getElementById("warning-list").innerHTML = "";
@@ -24,19 +26,43 @@ function loadFromGoogleSheets(data) {
   document.getElementById("info-section").innerHTML =
     "Loaded 0 of " + data.length + " locations";
 
-  data.forEach((item, index) => {
-    getMarkerWithGoogleSheetsData(item, geocoder, index)
-      .then(marker => {
-        marker.addTo(map);
-        const loadedItems = index + 1;
-        document.getElementById("info-section").innerHTML =
-          "Loaded " + loadedItems + " of " + data.length + " locations";
-      })
-      .catch(searchQuery => {
-        document.getElementById("warning-section").style.display = "";
-        const warningHtml = "♠ " + searchQuery + "<br/>";
-        document.getElementById("warning-list").innerHTML += warningHtml;
-      });
+  addMarkersToMap(data).then(() => {
+    L.control.layers({}, categories).addTo(map);
+    document.getElementById("info-section").innerHTML = "";
+  });
+}
+
+function addMarkersToMap(data) {
+  return new Promise((resolve, reject) => {
+    data.forEach((item, index) => {
+      if (item["Kategori"]) {
+        const categoryName = item["Kategori"];
+        if (!categories[categoryName]) {
+          categories[categoryName] = L.layerGroup([]);
+          // Add layer to map in order to show it by default:
+          map.addLayer(categories[categoryName]);
+        }
+      }
+      getMarkerWithGoogleSheetsData(item, geocoder, index)
+        .then(marker => {
+          if (item["Kategori"]) {
+            marker.addTo(categories[item["Kategori"]]);
+          } else {
+            marker.addTo(map);
+          }
+          const loadedItems = index + 1;
+          document.getElementById("info-section").innerHTML =
+            "Loaded " + loadedItems + " of " + data.length + " locations";
+          if (index === data.length - 1) {
+            resolve({});
+          }
+        })
+        .catch(searchQuery => {
+          document.getElementById("warning-section").style.display = "";
+          const warningHtml = "♠ " + searchQuery + "<br/>";
+          document.getElementById("warning-list").innerHTML += warningHtml;
+        });
+    });
   });
 }
 
