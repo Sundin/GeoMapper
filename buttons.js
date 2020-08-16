75524d6f480b73edd8a62e1ae7e88b2fd009b5f7
Gustav Sundin <sundin666@gmail.com>
2020-08-16 21:36:21 +0200

    Button to show all categories

--- buttons.js
+++ buttons.js
@@ -1,11 +1,19 @@
 $(function () {
   $("body").on("click", ".selectCategory", function (e) {
-    console.log("click", e.target.value);
     const selectedCategory = e.target.value;
 
     Object.keys(categories).forEach((category) => {
       map.removeLayer(categories[category]);
     });
     map.addLayer(categories[selectedCategory]);
+  });
+});
+
+$("body").on("click", ".showAllCategories", function () {
+  Object.keys(categories).forEach((category) => {
+    map.removeLayer(categories[category]);
+  });
+  Object.keys(categories).forEach((category) => {
+    map.addLayer(categories[category]);
   });
 });
